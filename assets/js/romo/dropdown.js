13a44c3954743172052df189a99e36bbe5291e9a
Kelly Redding <kelly@kellyredding.com>
2014-05-21 17:20:44 -0500

    dropdown: rework to dynamically add popup and position
    This reworks the dropdown component to add its popup and position
    it in the same fashion that the tooltip does.  This has the advantage
    of the popups being attached to the body to minimize unintentional
    styles and negating the need some relative positioning wrapper elem.
    
    This also generally cleans up the js and css based on the new design
    and some conventions set in the tooltip js.

--- assets/js/romo/dropdown.js
+++ assets/js/romo/dropdown.js
@@ -1,49 +1,43 @@
 $.fn.romoDropdown = function() {
   return $.map(this, function(elem) {
-    return new RomoDropdown($(elem));
+    return new RomoDropdown($(elem))
   })
 }
 
-// Dropdown
-
 var RomoDropdown = function(element) {
-  this.elem = $(element);
-  this.toggleElem = this.elem.find('> .romo-dropdown-toggle');
-  this.popupElem  = this.elem.find('> .romo-dropdown-popup');
-  this.bodyElem   = this.popupElem.find('> .romo-dropdown-body');
+  this.elem = $(element)
+  this.popupElem = $('<div class="romo-dropdown-popup"><div class="romo-dropdown-body"></div></div>')
+  this.popupElem.appendTo('body')
+  this.bodyElem = this.popupElem.find('> .romo-dropdown-body')
 
-  this.toggleElem.unbind('click');
-  this.toggleElem.on('click', $.proxy(this.onToggleClick, this));
-
+  var positionData = this._parsePositionData(this.elem.data('dropdown-position'))
+  this.popupPosition  = positionData.position  || 'bottom'
+  this.popupAlignment = positionData.alignment || 'left'
+  this.popupElem.attr('data-dropdown-position',  this.popupPosition)
+  this.popupElem.attr('data-dropdown-alignment', this.popupAlignment)
   // don't propagate click events on the popup elem.  this prevents the popup
   // from closing when clicked (see body click event bind on popup open)
   this.popupElem.on('click', function(e) {
     if (e != undefined) {
-      e.stopPropagation();
+      e.stopPropagation()
     }
   })
 
-  if (this.bodyElem.data('min-width') != undefined && this.elem.data('min-width') != '') {
-    this.bodyElem.css('min-width', this.bodyElem.data('min-width'));
-  }
-  if (this.bodyElem.data('max-width') != undefined && this.elem.data('max-width') != '') {
-    this.bodyElem.css('max-width', this.bodyElem.data('max-width'));
-  }
-  if (this.bodyElem.data('width') != undefined && this.elem.data('width') != '') {
-    this.bodyElem.css('width', this.bodyElem.data('width'));
-  }
-  if (this.bodyElem.data('min-height') != undefined && this.elem.data('min-height') != '') {
-    this.bodyElem.css('min-height', this.bodyElem.data('min-height'));
-  }
-  if (this.bodyElem.data('max-height') != undefined && this.elem.data('max-height') != '') {
-    this.bodyElem.css('max-height', this.bodyElem.data('max-height'));
-  }
-  if (this.bodyElem.data('height') != undefined && this.elem.data('height') != '') {
-    this.bodyElem.css('height', this.bodyElem.data('height'));
-  }
+  this.bodyElem.addClass(this.elem.data('dropdown-style-class'))
+  this.bodyElem.css({
+    'min-width':  this.elem.data('dropdown-min-width'),
+    'max-width':  this.elem.data('dropdown-max-width'),
+    'width':      this.elem.data('dropdown-width'),
+    'min-height': this.elem.data('dropdown-min-height'),
+    'max-height': this.elem.data('dropdown-max-height'),
+    'height':     this.elem.data('dropdown-height')
+  })
 
-  this.doInit();
-  this.elem.trigger('dropdown:onReady', [this]);
+  this.elem.unbind('click')
+  this.elem.on('click', $.proxy(this.onToggleClick, this))
+
+  this.doInit()
+  this.elem.trigger('dropdown:onReady', [this])
 }
 
 RomoDropdown.prototype.doInit = function() {
@@ -52,12 +46,15 @@ RomoDropdown.prototype.doInit = function() {
 
 RomoDropdown.prototype.doLoadBodyStart = function() {
   // override as needed
-  this.bodyElem.html('');
+  this.bodyElem.html('')
+  this.doPlacePopupElem()
 }
 
 RomoDropdown.prototype.doLoadBodySuccess = function(data) {
   // override as needed
-  this.bodyElem.html(data);
+  // TODO: initHtml
+  this.bodyElem.html(data)
+  this.doPlacePopupElem()
 }
 
 RomoDropdown.prototype.doLoadBodyError = function(xhr) {
@@ -66,30 +63,31 @@ RomoDropdown.prototype.doLoadBodyError = function(xhr) {
 
 RomoDropdown.prototype.onToggleClick = function(e) {
   if (e != undefined) {
-    e.preventDefault();
+    e.preventDefault()
   }
 
-  if (this.toggleElem.hasClass('disabled') == false) {
-    this.doToggle();
+  if (this.elem.hasClass('disabled') === false) {
+    this.doToggle()
   }
 }
 
 RomoDropdown.prototype.doToggle = function() {
   if (this.popupElem.hasClass('romo-dropdown-open')) {
-    this.doPopupClose();
+    this.doPopupClose()
   } else {
-    this.doPopupOpen();
+    this.doPopupOpen()
   }
-  this.elem.trigger('dropdown:onToggle', [this]);
+  this.elem.trigger('dropdown:onToggle', [this])
 }
 
 RomoDropdown.prototype.doPopupOpen = function() {
-  var loadHref = this.toggleElem.attr('href');
+  var loadHref = this.elem.attr('href')
   if (loadHref != undefined) {
-    this.doLoadBody(loadHref);
+    this.doLoadBody(loadHref)
   }
 
-  this.popupElem.addClass('romo-dropdown-open');
+  this.popupElem.addClass('romo-dropdown-open')
+  this.doPlacePopupElem()
 
   // bind an event to close the popup when clicking away from the
   // popup.  Bind on a timeout to allow time for any toggle
@@ -97,58 +95,100 @@ RomoDropdown.prototype.doPopupOpen = function() {
   // event, then the toggle click will propagate which will call
   // this event and immediately close the popup.
   setTimeout($.proxy(function() {
-    $('body').on('click', $.proxy(this.onWindowBodyClick, this));
-  }, this), 100);
+    $('body').on('click', $.proxy(this.onWindowBodyClick, this))
+  }, this), 100)
 
   // bind "esc" keystroke to toggle close
-  $('body').on('keyup', $.proxy(this.onWindowBodyKeyUp, this));
+  $('body').on('keyup', $.proxy(this.onWindowBodyKeyUp, this))
 
-  this.elem.trigger('dropdown:onPopupOpen', [this]);
+  // bind window resizes reposition dropdown
+  $(window).on('resize', $.proxy(this.onResizeWindow, this))
+
+  this.elem.trigger('dropdown:onPopupOpen', [this])
 }
 
 RomoDropdown.prototype.doPopupClose = function() {
-  this.popupElem.removeClass('romo-dropdown-open');
+  this.popupElem.removeClass('romo-dropdown-open')
 
   // unbind any event to close the popup when clicking away from it
-  $('body').off('click', $.proxy(this.onWindowBodyClick, this));
+  $('body').off('click', $.proxy(this.onWindowBodyClick, this))
 
   // unbind "esc" keystroke to toggle close
-  $('body').off('keyup', $.proxy(this.onWindowBodyKeyUp, this));
+  $('body').off('keyup', $.proxy(this.onWindowBodyKeyUp, this))
 
-  this.elem.trigger('dropdown:onPopupClose', [this]);
+  // unbind window resizes reposition dropdown
+  $(window).off('resize', $.proxy(this.onResizeWindow, this))
+
+  this.elem.trigger('dropdown:onPopupClose', [this])
 }
 
 RomoDropdown.prototype.onWindowBodyClick = function(e) {
-  this.doPopupClose();
+  this.doPopupClose()
 }
 
 RomoDropdown.prototype.onWindowBodyKeyUp = function(e) {
   if (e.keyCode == 27) { // esc
-    this.doPopupClose();
+    this.doPopupClose()
   }
 }
 
+RomoDropdown.prototype.onResizeWindow = function(e) {
+  this.doPlacePopupElem()
+}
+
 RomoDropdown.prototype.doLoadBody = function(href) {
-  this.doLoadBodyStart();
-  this.elem.trigger('dropdown:onLoadBodyStart', [this]);
+  this.doLoadBodyStart()
+  this.elem.trigger('dropdown:onLoadBodyStart', [this])
 
   $.ajax({
     url: href,
     success: $.proxy(this.onLoadBodyAjaxSuccess, this),
     error: $.proxy(this.onLoadBodyAjaxError, this)
-  });
+  })
 }
 
 RomoDropdown.prototype.onLoadBodyAjaxSuccess = function(data, status, xhr) {
-  this.doLoadBodySuccess(data);
-  this.elem.trigger('dropdown:onLoadBodySuccess', [data, this]);
+  this.doLoadBodySuccess(data)
+  this.elem.trigger('dropdown:onLoadBodySuccess', [data, this])
 }
 
 RomoDropdown.prototype.onLoadBodyAjaxError = function(xhr, errorType, error) {
-  this.doLoadBodyError(xhr);
-  this.elem.trigger('dropdown:onLoadBodyError', [xhr, this]);
+  this.doLoadBodyError(xhr)
+  this.elem.trigger('dropdown:onLoadBodyError', [xhr, this])
+}
+
+RomoDropdown.prototype.doPlacePopupElem = function() {
+  var pos = $.extend({}, this.elem[0].getBoundingClientRect(), this.elem.offset())
+  var w = this.popupElem[0].offsetWidth
+  var h = this.popupElem[0].offsetHeight
+  var pad = 2 // spacing
+  var offset = {}
+
+  switch (this.popupPosition) {
+    case 'top':
+      $.extend(offset, { top: pos.top - h - pad })
+      break
+    case 'bottom':
+      $.extend(offset, { top: pos.top + pos.height + pad })
+      break
+  }
+  switch (this.popupAlignment) {
+    case 'left':
+      $.extend(offset, { left: pos.left })
+      break
+    case 'right':
+      $.extend(offset, { left: pos.right - w })
+      break
+  }
+
+  this.popupElem.offset(offset)
+}
+
+RomoDropdown.prototype._parsePositionData = function(posString) {
+  var posData = (posString || '').split(',')
+  return { position: posData[0], alignment: posData[1] }
 }
 
 Romo.onInitUI(function(e) {
-  $(e.target).find('.romo-dropdown[data-romo-auto="true"]').romoDropdown();
-});
+  $(e.target).find('.romo-dropdown[data-dropdown-auto="true"]').romoDropdown()
+})
