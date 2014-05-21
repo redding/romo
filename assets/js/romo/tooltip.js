57975e3733f9c83ca2fdd99b457e8d449c22f788
Kelly Redding <kelly@kellyredding.com>
2014-05-21 08:22:51 -0500

    tooltip: rewrite to dynamically build the popup
    This switches the tooltip implementation to dynamically build the
    popup element and attach it to the body.  The benefit here is the
    probability for unintentional styling goes way down and there is
    less markup for the developer to add.
    
    The downside of this approach is that we now have to absolutely
    position the popup relative the the toggle element.  This involves
    doing the math to center-position it based on the given position
    attr and then binding to window resizes to adjust the position.
    This also assumes the tooltip is show and has its final content
    in place before positioning it.  This also means we have to re-
    position the tooltip every time it is opened.
    
    Using this approach eleminates most of the edge-case scenarios of
    the previous approach and allows for a better developer experience.

--- assets/js/romo/tooltip.js
+++ assets/js/romo/tooltip.js
@@ -1,86 +1,55 @@
 $.fn.romoTooltip = function() {
   return $.map(this, function(elem) {
-    return new RomoTooltip($(elem));
+    return new RomoTooltip($(elem))
   })
 }
 
-// Tooltip
-
 var RomoTooltip = function(element) {
-  this.elem = $(element);
-  this.toggleElem = this.elem.find('> .romo-tooltip-toggle');
-  this.popupElem  = this.elem.find('> .romo-tooltip-popup');
-  this.bodyElem   = this.popupElem.find('> .romo-tooltip-body');
-
-  if (this.elem.data('romo-display') != undefined && this.elem.data('romo-display') != '') {
-    this.elem.css('display', this.elem.data('romo-display'));
-    this.toggleElem.css('display', this.elem.data('romo-display'));
-  }
+  this.elem = $(element)
+  this.popupElem = $('<div class="romo-tooltip-popup"><div class="romo-tooltip-arrow"></div><div class="romo-tooltip-body"></div></div>')
+  this.popupElem.appendTo('body')
+  this.arrowElem = this.popupElem.find('> .romo-tooltip-arrow')
+  this.bodyElem  = this.popupElem.find('> .romo-tooltip-body')
 
   this.hoverState = 'out'
   this.delayEnter = 0
   this.delayLeave = 0
-
-  if (this.toggleElem.data('delay') != undefined && this.toggleElem.data('delay') != '') {
-    this.delayEnter = this.toggleElem.data('delay');
-    this.delayLeave = this.toggleElem.data('delay');
+  if (this.elem.data('tooltip-delay') != undefined && this.elem.data('tooltip-delay') != '') {
+    this.delayEnter = this.elem.data('tooltip-delay')
+    this.delayLeave = this.elem.data('tooltip-delay')
   }
-  if (this.toggleElem.data('delay-enter') != undefined && this.toggleElem.data('delay-enter') != '') {
-    this.delayEnter = this.toggleElem.data('delay-enter');
+  if (this.elem.data('tooltip-delay-enter') != undefined && this.elem.data('tooltip-delay-enter') != '') {
+    this.delayEnter = this.elem.data('tooltip-delay-enter')
   }
-  if (this.toggleElem.data('delay-leave') != undefined && this.toggleElem.data('delay-leave') != '') {
-    this.delayLeave = this.toggleElem.data('delay-leave');
-  }
-
-  if (this.toggleElem.data('disabled') != true) {
-    this.toggleElem.on('mouseenter', $.proxy(this.onToggleEnter, this));
-    this.toggleElem.on('mouseleave', $.proxy(this.onToggleLeave, this));
+  if (this.elem.data('tooltip-delay-leave') != undefined && this.elem.data('tooltip-delay-leave') != '') {
+    this.delayLeave = this.elem.data('tooltip-delay-leave')
   }
 
+  this.popupPosition = this.elem.data('tooltip-position') || 'top'
+  this.popupElem.attr('data-tooltip-position', this.popupPosition)
+  this.elem.on('mouseenter', $.proxy(this.onToggleEnter, this))
+  this.elem.on('mouseleave', $.proxy(this.onToggleLeave, this))
+  $(window).on('resize', $.proxy(this.onResizeWindow, this))
   // don't propagate click events on the popup elem.  this prevents the popup
   // from closing when clicked (see body click event bind on popup open)
   this.popupElem.on('click', function(e) {
     if (e != undefined) {
-      e.stopPropagation();
+      e.stopPropagation()
     }
   })
 
-  if (this.popupElem.data('align-adjust') != undefined && this.popupElem.data('align-adjust') != '') {
-    if (this.popupElem.data('align') === 'left') {
-      this.popupElem.css('left', this.popupElem.data('align-adjust'));
-    }
-    else if (this.popupElem.data('align') === 'top') {
-      this.popupElem.css('top', this.popupElem.data('align-adjust'));
-    }
-    else if (this.popupElem.data('align') === 'right') {
-      this.popupElem.css('right', this.popupElem.data('align-adjust'));
-    }
-    else if (this.popupElem.data('align') === 'bottom') {
-      this.popupElem.css('bottom', this.popupElem.data('align-adjust'));
-    }
-  }
+  this.bodyElem.addClass(this.elem.data('tooltip-style-class'))
+  this.bodyElem.css('min-width', this.elem.data('tooltip-min-width'))
+  this.bodyElem.css('max-width', this.elem.data('tooltip-max-width'))
+  this.bodyElem.css('width', this.elem.data('tooltip-width'))
+  this.bodyElem.css('min-height', this.elem.data('tooltip-min-height'))
+  this.bodyElem.css('max-height', this.elem.data('tooltip-max-height'))
+  this.bodyElem.css('height', this.elem.data('tooltip-height'))
+  this.bodyElem.html(this.elem.data('tooltip-content') || '')
+  // TODO: tooltip-href handling
 
-  if (this.bodyElem.data('min-width') != undefined && this.elem.data('min-width') != '') {
-    this.bodyElem.css('min-width', this.bodyElem.data('min-width'));
-  }
-  if (this.bodyElem.data('max-width') != undefined && this.elem.data('max-width') != '') {
-    this.bodyElem.css('max-width', this.bodyElem.data('max-width'));
-  }
-  if (this.bodyElem.data('width') != undefined && this.elem.data('width') != '') {
-    this.bodyElem.css('width', this.bodyElem.data('width'));
-  }
-  if (this.bodyElem.data('min-height') != undefined && this.elem.data('min-height') != '') {
-    this.bodyElem.css('min-height', this.bodyElem.data('min-height'));
-  }
-  if (this.bodyElem.data('max-height') != undefined && this.elem.data('max-height') != '') {
-    this.bodyElem.css('max-height', this.bodyElem.data('max-height'));
-  }
-  if (this.bodyElem.data('height') != undefined && this.elem.data('height') != '') {
-    this.bodyElem.css('height', this.bodyElem.data('height'));
-  }
-
-  this.doInit();
-  this.elem.trigger('tooltip:onReady', [this]);
+  this.doInit()
+  this.elem.trigger('tooltip:onReady', [this])
 }
 
 RomoTooltip.prototype.doInit = function() {
@@ -89,48 +58,80 @@ RomoTooltip.prototype.doInit = function() {
 
 RomoTooltip.prototype.onToggleEnter = function(e) {
   if (e != undefined) {
-    e.preventDefault();
+    e.preventDefault()
   }
 
-  this.hoverState = 'in';
-  if (this.toggleElem.hasClass('disabled') == false) {
-    clearTimeout(this.timeout);
+  this.hoverState = 'in'
+  if (this.elem.hasClass('disabled') === false) {
+    clearTimeout(this.timeout)
     this.timeout = setTimeout($.proxy(function() {
-      if (this.hoverState == 'in') {
-        this.doPopupOpen();
+      if (this.hoverState ==='in') {
+        this.doPopupOpen()
       }
-    }, this), this.delayEnter);
+    }, this), this.delayEnter)
   }
 }
 
 RomoTooltip.prototype.onToggleLeave = function(e) {
   if (e != undefined) {
-    e.preventDefault();
+    e.preventDefault()
   }
 
-  this.hoverState = 'out';
-  if (this.toggleElem.hasClass('disabled') == false) {
-    clearTimeout(this.timeout);
+  this.hoverState = 'out'
+  if (this.elem.hasClass('disabled') === false) {
+    clearTimeout(this.timeout)
     this.timeout = setTimeout($.proxy(function() {
-      if (this.hoverState == 'out') {
-        this.doPopupClose();
+      if (this.hoverState === 'out') {
+        this.doPopupClose()
       }
-    }, this), this.delayLeave);
+    }, this), this.delayLeave)
   }
 }
 
+RomoTooltip.prototype.onResizeWindow = function(e) {
+  if (this.elem.hasClass('disabled') === false && this.hoverState === 'in') {
+    this.doPlacePopupElem()
+  }
+}
 RomoTooltip.prototype.doPopupOpen = function() {
-  this.popupElem.addClass('romo-tooltip-open');
+  this.popupElem.addClass('romo-tooltip-open')
+  this.doPlacePopupElem()
 
-  this.elem.trigger('tooltip:onPopupOpen', [this]);
+  this.elem.trigger('tooltip:onPopupOpen', [this])
 }
 
 RomoTooltip.prototype.doPopupClose = function() {
-  this.popupElem.removeClass('romo-tooltip-open');
+  this.popupElem.removeClass('romo-tooltip-open')
 
-  this.elem.trigger('tooltip:onPopupClose', [this]);
+  this.elem.trigger('tooltip:onPopupClose', [this])
+}
+
+RomoTooltip.prototype.doPlacePopupElem = function() {
+  var pos = $.extend({}, this.elem[0].getBoundingClientRect(), this.elem.offset())
+  var w = this.popupElem[0].offsetWidth
+  var h = this.popupElem[0].offsetHeight
+
+  var pad = 6 + 1 // arrow size + spacing
+  var offset = {}
+
+  switch (this.popupPosition) {
+    case 'top':
+      $.extend(offset, { top: pos.top - h - pad, left: pos.left + pos.width / 2 - w / 2 })
+      break
+    case 'bottom':
+      $.extend(offset, { top: pos.top + pos.height + pad, left: pos.left + pos.width / 2 - w / 2 })
+      break
+    case 'left':
+      $.extend(offset, { top: pos.top + pos.height / 2 - h / 2, left: pos.left - w - pad })
+      break
+    case 'right':
+      $.extend(offset, { top: pos.top + pos.height / 2 - h / 2, left: pos.left + pos.width + pad })
+      break
+  }
+
+  this.popupElem.offset(offset)
 }
 
 Romo.onInitUI(function(e) {
-  $(e.target).find('.romo-tooltip[data-romo-auto="true"]').romoTooltip();
-});
+  $(e.target).find('.romo-tooltip[data-tooltip-auto="true"]').romoTooltip()
+})
