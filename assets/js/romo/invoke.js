55c4e67bf58b57008907c953a46536df3aa5dbf5
Kelly Redding <kelly@kellyredding.com>
2014-05-27 12:21:23 -0500

    invoke component
    This adds a new component for invoking action on a click event on
    a given element.  It also handles optionally loading data via a
    given `href` attribute on the invoke element.
    
    This is a common pattern in Romo components: have a link/button
    that should, on click, make an ajax call to some href and do something
    with the data.  This pattern is used on the Dropdown component and
    will be used on the coming Inline and Modal components.  This may
    be used eventually on tooltips that load dynamic content some day
    as well.
    
    This is prep for adding the Inline and Modal components, specifically.

---
+++ assets/js/romo/invoke.js
@@ -0,0 +1,68 @@
+$.fn.romoInvoke = function() {
+  return $.map(this, function(elem) {
+    return new RomoInvoke($(elem))
+  })
+}
+
+var RomoInvoke = function(element) {
+  this.elem = $(element)
+  this.targetElem = $(this.elem.data('invoke-target'))
+
+  this.elem.unbind('click')
+  this.elem.on('click', $.proxy(this.onClick, this))
+
+  this.doInit()
+  this._trigger('invoke:onReady', [this])
+}
+
+RomoInvoke.prototype.doInit = function() {
+  // override as needed
+}
+
+RomoInvoke.prototype.onClick = function(e) {
+  if (e != undefined) {
+    e.preventDefault()
+  }
+
+  if (this.elem.hasClass('disabled') === false) {
+    this.doInvoke()
+  }
+}
+
+RomoInvoke.prototype.doInvoke = function() {
+  var loadHref = this.elem.attr('href')
+  if (loadHref != undefined) {
+    this.doLoad(loadHref)
+  }
+
+  this._trigger('invoke:onInvoke', [this]);
+}
+
+RomoInvoke.prototype.doLoad = function(href) {
+  this._trigger('invoke:onLoadStart', [this]);
+
+  $.ajax({
+    url:     href,
+    success: $.proxy(this.onLoadAjaxSuccess, this),
+    error:   $.proxy(this.onLoadAjaxError,   this)
+  })
+}
+
+RomoInvoke.prototype.onLoadAjaxSuccess = function(data, status, xhr) {
+  this._trigger('invoke:onLoadSuccess', [data, this]);
+}
+
+RomoInvoke.prototype.onLoadAjaxError = function(xhr, errorType, error) {
+  this._trigger('invoke:onLoadError', [xhr, this]);
+}
+
+RomoInvoke.prototype._trigger = function(event_name, event_data) {
+  this.elem.trigger(event_name, event_data)
+  if (this.targetElem != undefined) {
+    this.targetElem.trigger(event_name, event_data)
+  }
+}
+
+Romo.onInitUI(function(e) {
+  $(e.target).find('.romo-invoke[data-invoke-auto="true"]').romoInvoke()
+})
