befe778119472ab44c67dc4581ee77ac1e9e3864
Kelly Redding <kelly@kellyredding.com>
2014-06-03 10:29:47 -0500

    inline: first pass at the component
    This adds a new "inline" js component.  An inline component toggles
    inline UI.  This could include things like a show more/less, inline
    editing UI, etc.
    
    The inline is setup to work as an invoke target - by that I mean
    inlines can be "invoked" just like dropdowns are "invoked".  Right
    now it simply handles toggling UI and working with the invoke
    component.

---
+++ assets/js/romo/inline.js
@@ -0,0 +1,74 @@
+$.fn.romoInline = function() {
+  return $.map(this, function(elem) {
+    return new RomoInline($(elem))
+  })
+}
+
+var RomoInline = function(element) {
+  this.elem = $(element)
+  this.toggleElem = $(this.elem.data('romo-inline-toggle'))
+
+  this.elem.on('invoke:invoke', $.proxy(function(e, invoke) {
+    this.doInvoke()
+  }, this))
+  this.elem.on('invoke:loadStart', $.proxy(function(e, invoke) {
+    this.doLoadStart()
+  }, this))
+  this.elem.on('invoke:loadSuccess', $.proxy(function(e, data, invoke) {
+    this.doLoadSuccess(data)
+  }, this))
+  this.elem.on('invoke:loadError', $.proxy(function(e, xhr, invoke) {
+    this.doLoadError(xhr)
+  }, this))
+
+  this.doInit()
+  this.elem.trigger('inline:ready', [this])
+}
+
+RomoInline.prototype.doInit = function() {
+  // override as needed
+}
+
+RomoInline.prototype.doInvoke = function() {
+  this.elem.show()
+  this.toggleElem.hide()
+  this.elem.trigger('inline:invoke', [this])
+}
+
+RomoInline.prototype.doLoadStart = function() {
+  this.elem.html('')
+  this.elem.trigger('inline:loadStart', [this])
+}
+
+RomoInline.prototype.doLoadSuccess = function(data) {
+  Romo.initHtml(this.elem, data)
+
+  var dismissElem = this.elem.find('[data-romo-inline-dismiss="true"]')
+  dismissElem.unbind('click')
+  dismissElem.on('click', $.proxy(this.onDismissClick, this))
+
+  this.elem.trigger('inline:loadSuccess', [data, this])
+}
+
+RomoInline.prototype.doLoadError = function(xhr) {
+  this.elem.trigger('inline:loadError', [xhr, this])
+}
+
+RomoInline.prototype.onDismissClick = function(e) {
+  if (e !== undefined) {
+    e.preventDefault()
+  }
+
+  this.doDismiss()
+}
+
+RomoInline.prototype.doDismiss = function() {
+  this.toggleElem.show()
+  this.elem.hide()
+  this.elem.trigger('inline:dismiss', [this])
+}
+
+Romo.onInitUI(function(e) {
+  $(e.target).find('[data-romo-inline-auto="true"]').romoInline()
+})
+
