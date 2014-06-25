0df7644a9acd51c13f1c0d3cde899edaa6b726ee
Kelly Redding <kelly@kellyredding.com>
2014-06-25 14:03:00 -0500

    modalForm: first pass, modeled after inlineForm
    This creates a modalForm component that works just as the inlineForm
    does.  It finds together a form and a modal and helps by proxying
    events and making a single element feel like both a form and a modal.

---
+++ assets/js/romo/modal_form.js
@@ -0,0 +1,100 @@
+$.fn.romoModalForm = function() {
+  return $.map(this, function(element) {
+    return new RomoModalForm(element);
+  });
+}
+
+var RomoModalForm = function(element) {
+  this.elem = $(element);
+
+  this.modal = this.elem.romoModal()[0];
+  this.doBindModal();
+
+  this.form = undefined;
+  this.elem.on('modalForm:form:triggerSubmit', $.proxy(function(e) {
+    if (this.form != undefined) {
+      this.form.elem.trigger('form:triggerSubmit', []);
+    }
+  }, this));
+  this.doBindForm();
+  this.elem.on('modal:loadBodySuccess', $.proxy(function(e, data, modal) {
+    this.doBindForm();
+  }, this));
+
+  this.doInit();
+  this.elem.trigger('modalForm:ready', [this]);
+}
+
+RomoModalForm.prototype.doInit = function() {
+  // override as needed
+}
+
+RomoModalForm.prototype.doBindModal = function() {
+  this.elem.on('modal:ready', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:ready', [modal, this]);
+  }, this));
+  this.elem.on('modal:toggle', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:toggle', [modal, this]);
+  }, this));
+  this.elem.on('modal:popupOpen', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:popupOpen', [modal, this]);
+  }, this));
+  this.elem.on('modal:popupClose', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:popupClose', [modal, this]);
+  }, this));
+  this.elem.on('modal:dragStart', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:dragStart', [modal, this]);
+  }, this));
+  this.elem.on('modal:dragMove', $.proxy(function(e, placeX, placeY, modal) {
+    this.elem.trigger('modalForm:modal:dragMove', [placeX, placeY, modal, this]);
+  }, this));
+  this.elem.on('modal:dragStop', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:dragStop', [modal, this]);
+  }, this));
+  this.elem.on('modal:loadBodyStart', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:loadBodyStart', [modal, this]);
+  }, this));
+  this.elem.on('modal:loadBodySuccess', $.proxy(function(e, data, modal) {
+    this.elem.trigger('modalForm:modal:loadBodySuccess', [data, modal, this]);
+  }, this));
+  this.elem.on('modal:loadBodyError', $.proxy(function(e, xhr, modal) {
+    this.elem.trigger('modalForm:modal:loadBodyError', [xhr, modal, this]);
+  }, this));
+  this.elem.on('modal:dismiss', $.proxy(function(e, modal) {
+    this.elem.trigger('modalForm:modal:dismiss', [modal, this]);
+  }, this));
+}
+
+RomoModalForm.prototype.doBindForm = function() {
+  var formElem = this.modal.popupElem.find('[data-romo-form-auto="modalForm"]');
+
+  formElem.on('form:clearMsgs', $.proxy(function(e, form) {
+    this.elem.trigger('modalForm:form:clearMsgs', [form, this]);
+  }, this));
+  formElem.on('form:ready', $.proxy(function(e, form) {
+    this.elem.trigger('modalForm:form:ready', [form, this]);
+  }, this));
+  formElem.on('form:beforeSubmit', $.proxy(function(e, form) {
+    this.elem.trigger('modalForm:form:beforeSubmit', [form, this]);
+  }, this));
+  formElem.on('form:submitSuccess', $.proxy(function(e, data, form) {
+    this.elem.trigger('modalForm:form:submitSuccess', [data, form, this]);
+  }, this));
+  formElem.on('form:submitInvalidMsgs', $.proxy(function(e, msgs, xhr, form) {
+    this.elem.trigger('modalForm:form:submitInvalidMsgs', [msgs, xhr, form, this]);
+  }, this));
+  formElem.on('form:submitXhrError', $.proxy(function(e, xhr, form) {
+    this.elem.trigger('modalForm:form:submitXhrError', [xhr, form, this]);
+  }, this));
+  formElem.on('form:submitError', $.proxy(function(e, xhr, form) {
+    this.elem.trigger('modalForm:form:submitError', [xhr, form, this]);
+  }, this));
+
+  var submitElement = this.modal.popupElem.find('[data-romo-form-submit="true"]')[0];
+  this.form = formElem.romoForm(submitElement)[0];
+}
+
+Romo.onInitUI(function(e) {
+  $(e.target).find('[data-romo-modalForm-auto="true"]').romoModalForm();
+});
+
