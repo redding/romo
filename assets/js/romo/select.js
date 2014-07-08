a46d2bd7b61ae96487f126057022f70803f90dd6
Kelly Redding <kelly@kellyredding.com>
2014-07-08 12:53:13 -0500

    select: add icon for caret
    This adds settings to specify an icon to use as a caret.  This also
    adds the css/js to handle positioning styling the caret.
    
    This uses a graduated scale percentage width scheme to position
    the caret.  This has the advantage of being able to use vertical
    align middle styling to keep things lined up nice.  The graduated
    scale makes positioning work at extremely small and large sizes.

--- assets/js/romo/select.js
+++ assets/js/romo/select.js
@@ -6,6 +6,7 @@ $.fn.romoSelect = function() {
 
 var RomoSelect = function(element) {
   this.elem = $(element);
+  this.defaultCaretClass = '';
 
   this.doInit();
   this.romoDropdown = this._buildDropdownElem().romoDropdown()[0];
@@ -47,13 +48,13 @@ RomoSelect.prototype.doSelect = function() {
 }
 
 RomoSelect.prototype._buildDropdownElem = function() {
-  var romoDropdownElem = $('<button class="romo-select">Selected Goes Here</button>');
+  var romoDropdownElem = $('<div class="romo-select"><span>Something long that will overflow</span><i class=""></i></div>');
 
-  romoDropdownElem.attr('data-romo-dropdown-position', this.elem.data('romo-dropdown-position'));
-  romoDropdownElem.attr('data-romo-dropdown-style-class', this.elem.data('romo-dropdown-style-class'));
-  romoDropdownElem.attr('data-romo-dropdown-min-height', this.elem.data('romo-dropdown-min-height'));
-  romoDropdownElem.attr('data-romo-dropdown-max-height', this.elem.data('romo-dropdown-max-height'));
-  romoDropdownElem.attr('data-romo-dropdown-height', this.elem.data('romo-dropdown-height'));
+  romoDropdownElem.attr('data-romo-dropdown-position', this.elem.data('romo-select-dropdown-position'));
+  romoDropdownElem.attr('data-romo-dropdown-style-class', this.elem.data('romo-select-dropdown-style-class'));
+  romoDropdownElem.attr('data-romo-dropdown-min-height', this.elem.data('romo-select-dropdown-min-height'));
+  romoDropdownElem.attr('data-romo-dropdown-max-height', this.elem.data('romo-select-dropdown-max-height'));
+  romoDropdownElem.attr('data-romo-dropdown-height', this.elem.data('romo-select-dropdown-height'));
   // ignore any dropdown width settings - will match width of select elem
 
   var classList = this.elem.attr('class') !== undefined ? this.elem.attr('class').split(/\s+/) : [];
@@ -67,7 +68,21 @@ RomoSelect.prototype._buildDropdownElem = function() {
     romoDropdownElem.attr('style', this.elem.attr('style'));
   }
 
-  romoDropdownElem.css({width: this.elem.css('width')});
+  romoDropdownElem.css({'width': this.elem.css('width')});
+  var span = romoDropdownElem.find('> span');
+  var icon = romoDropdownElem.find('> i');
+  var width = parseInt(romoDropdownElem.css('width'));
+  if (width < 100) {
+    span.css({'width': '70%', 'padding-left': '10%'});
+    icon.css({'width': '30%', 'padding-right': '10%'});
+  } else if (width < 200) {
+    span.css({'width': '80%', 'padding-left': '5%'});
+    icon.css({'width': '20%', 'padding-right': '5%'});
+  } else {
+    span.css({'width': '90%', 'padding-left': '5%'});
+    icon.css({'width': '10%', 'padding-right': '5%'});
+  }
+  icon.addClass(this.elem.data('romo-select-caret-class') || this.defaultCaretClass);
 
   if (this.elem.attr('disabled') !== undefined) {
     romoDropdownElem.attr('disabled', this.elem.attr('disabled'));
