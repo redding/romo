2318066cd34bc28641b74a951c4901166006c9df
Kelly Redding <kelly@kellyredding.com>
2017-09-25 11:11:08 -0500

    (hotfix) restructure base js Romo class definition
    This removes an unnecessary function wrapper.  This is a legacy
    convention from jQuery usage.  This is prep for adding a bunch
    of base methods to emulate the parts of jQuery's api that Romo
    uses.  That is prep for removing jQuery as a dependency for Romo.

--- assets/js/romo/base.js
+++ assets/js/romo/base.js
@@ -1,273 +1,269 @@
-(function() {
-  var Romo = function() {
-    this._eventCallbacks = [];
+var Romo = function() {
+  this._eventCallbacks = [];
+}
+
+Romo.prototype.doInit = function() {
+  this.parentChildElems = new RomoParentChildElems();
+
+  $.each(this._eventCallbacks, function(idx, eventCallback) {
+    $('body').on(eventCallback.eventName, eventCallback.callback);
+  });
+
+  this.triggerInitUI($('body'));
+}
+
+// init UI
+
+Romo.prototype.onInitUI = function(callback) {
+  this._addEventCallback('romo:initUI', callback);
+}
+
+Romo.prototype.triggerInitUI = function(elems) {
+  elems.trigger('romo:initUI');
+}
+
+Romo.prototype.initUIElems = function(e, selector) {
+  var elems = $(e.target).find(selector).get();
+  if ($(e.target).is(selector)) {
+    elems.push(e.target)
+  }
+  return $(elems);
+}
+
+Romo.prototype.initHtml = function(elems, data) {
+  elems.each($.proxy(function(index, elem) {
+    var htmlElems = $(data)
+    $(elem).html(htmlElems);
+    this.triggerInitUI(htmlElems);
+  }, this));
+}
+
+Romo.prototype.initReplace = function(elems, data) {
+  elems.each($.proxy(function(index, elem) {
+    var replacementElem = $(data);
+    $(elem).replaceWith(replacementElem);
+    this.triggerInitUI(replacementElem);
+  }, this));
+}
+
+Romo.prototype.initPrepend = function(elems, data) {
+  elems.each($.proxy(function(index, elem) {
+    var prependedElem = $(data);
+    $(elem).prepend(prependedElem);
+    this.triggerInitUI(prependedElem);
+  }, this));
+}
+
+Romo.prototype.initAppend = function(elems, data) {
+  elems.each($.proxy(function(index, elem) {
+    var appendedElem = $(data);
+    $(elem).append(appendedElem);
+    this.triggerInitUI(appendedElem);
+  }, this));
+}
+
+Romo.prototype.initBefore = function(elems, data) {
+  elems.each($.proxy(function(index, elem) {
+    var insertedElem = $(data);
+    $(elem).before(insertedElem);
+    this.triggerInitUI(insertedElem);
+  }, this));
+}
+
+Romo.prototype.initAfter = function(elems, data) {
+  elems.each($.proxy(function(index, elem) {
+    var insertedElem = $(data);
+    $(elem).after(insertedElem);
+    this.triggerInitUI(insertedElem);
+  }, this));
+}
+
+// page handling
+
+Romo.prototype.reloadPage = function() {
+  window.location = window.location;
+}
+
+Romo.prototype.redirectPage = function(redirectUrl) {
+  window.location = redirectUrl;
+}
+
+// param serialization
+
+Romo.prototype.param = function(data, opts) {
+  var paramData = $.extend({}, data);
+
+  if (opts && opts.removeEmpty) {
+    $.each(paramData, function(key, value) {
+      if (value === '') {
+        delete paramData[key];
+      }
+    })
   }
 
-  Romo.prototype.doInit = function() {
-    this.parentChildElems = new RomoParentChildElems();
+  var paramString = $.param(paramData);
 
-    $.each(this._eventCallbacks, function(idx, eventCallback) {
-      $('body').on(eventCallback.eventName, eventCallback.callback);
-    });
-
-    this.triggerInitUI($('body'));
+  if (opts && opts.decodeValues) {
+    paramString = this.decodeParam(paramString);
   }
 
-  // init UI
+  return paramString;
+}
 
-  Romo.prototype.onInitUI = function(callback) {
-    this._addEventCallback('romo:initUI', callback);
+Romo.prototype.decodeParam = function(string) {
+  return this.decodeParamMap.reduce(function(prev, curr) {
+    return prev.replace(curr[0], curr[1]);
+  }, string);
+}
+
+Romo.prototype.decodeParamMap = [
+  [/%20/g, '+'],
+  [/%21/g, '!'],
+  [/%24/g, '$'],
+  [/%28/g, '('],
+  [/%29/g, ')'],
+  [/%2A/g, '*'],
+  [/%2B/g, '+'],
+  [/%2C/g, ','],
+  [/%2D/g, '-'],
+  [/%2E/g, '.'],
+  [/%2F/g, '/'],
+  [/%5B/g, '['],
+  [/%5C/g, '\\'],
+  [/%5D/g, ']'],
+  [/%3A/g, ':'],
+  [/%3C/g, '<'],
+  [/%3E/g, '>'],
+  [/%3F/g, '?'],
+  [/%40/g, '@'],
+  [/%5E/g, '^'],
+  [/%5F/g, '_'],
+  [/%60/g, '`'],
+  [/%7B/g, '{'],
+  [/%7C/g, '|'],
+  [/%7D/g, '}'],
+  [/%7E/g, '~']
+];
+
+// style handling
+
+Romo.prototype.getComputedStyle = function(node, styleName) {
+  return window.getComputedStyle(node, null).getPropertyValue(styleName);
+}
+
+Romo.prototype.parseZIndex = function(elem) {
+  // for the case where z-index is set directly on the elem
+  var val = this.parseElemZIndex(elem);
+  if (val !== 0) {
+    return val;
   }
 
-  Romo.prototype.triggerInitUI = function(elems) {
-    elems.trigger('romo:initUI');
-  }
-
-  Romo.prototype.initUIElems = function(e, selector) {
-    var elems = $(e.target).find(selector).get();
-    if ($(e.target).is(selector)) {
-      elems.push(e.target)
+  // for the case where z-index is inherited from a parent elem
+  var parentIndexes = this.toArray(elem.parents()).reduce($.proxy(function(prev, curr) {
+    var pval = this.parseElemZIndex($(curr));
+    if (pval !== 0) {
+      prev.push(pval);
     }
-    return $(elems);
+    return prev;
+  }, this), []);
+  parentIndexes.push(0); // in case z-index is 'auto' all the way up
+  return parentIndexes[0];
+}
+
+Romo.prototype.parseElemZIndex = function(elem) {
+  var val = parseInt(this.getComputedStyle(elem[0], "z-index"));
+  if (!isNaN(val)) {
+    return val;
   }
+  return 0;
+}
 
-  Romo.prototype.initHtml = function(elems, data) {
-    elems.each($.proxy(function(index, elem) {
-      var htmlElems = $(data)
-      $(elem).html(htmlElems);
-      this.triggerInitUI(htmlElems);
-    }, this));
-  }
+// elem handling
 
-  Romo.prototype.initReplace = function(elems, data) {
-    elems.each($.proxy(function(index, elem) {
-      var replacementElem = $(data);
-      $(elem).replaceWith(replacementElem);
-      this.triggerInitUI(replacementElem);
-    }, this));
-  }
+Romo.prototype.toArray = function(elems) {
+  // converts a collection of elements `$()` to an array of nodes
+  return $.map(elems, function(node){ return node; })
+}
 
-  Romo.prototype.initPrepend = function(elems, data) {
-    elems.each($.proxy(function(index, elem) {
-      var prependedElem = $(data);
-      $(elem).prepend(prependedElem);
-      this.triggerInitUI(prependedElem);
-    }, this));
-  }
-
-  Romo.prototype.initAppend = function(elems, data) {
-    elems.each($.proxy(function(index, elem) {
-      var appendedElem = $(data);
-      $(elem).append(appendedElem);
-      this.triggerInitUI(appendedElem);
-    }, this));
-  }
-
-  Romo.prototype.initBefore = function(elems, data) {
-    elems.each($.proxy(function(index, elem) {
-      var insertedElem = $(data);
-      $(elem).before(insertedElem);
-      this.triggerInitUI(insertedElem);
-    }, this));
-  }
-
-  Romo.prototype.initAfter = function(elems, data) {
-    elems.each($.proxy(function(index, elem) {
-      var insertedElem = $(data);
-      $(elem).after(insertedElem);
-      this.triggerInitUI(insertedElem);
-    }, this));
-  }
-
-  // page handling
-
-  Romo.prototype.reloadPage = function() {
-    window.location = window.location;
-  }
-
-  Romo.prototype.redirectPage = function(redirectUrl) {
-    window.location = redirectUrl;
-  }
-
-  // param serialization
-
-  Romo.prototype.param = function(data, opts) {
-    var paramData = $.extend({}, data);
-
-    if (opts && opts.removeEmpty) {
-      $.each(paramData, function(key, value) {
-        if (value === '') {
-          delete paramData[key];
-        }
-      })
+Romo.prototype.selectNext = function(elem, selector) {
+  // like `$().next()`, but takes a selector; returns first next elem that
+  // matches the given selector or an empty collection if non matches
+  var el = elem.next();
+  while(el.length) {
+    if (selector === undefined || el.is(selector)) {
+      return el;
     }
+    el = el.next();
+  }
+  return el;
+}
 
-    var paramString = $.param(paramData);
-
-    if (opts && opts.decodeValues) {
-      paramString = this.decodeParam(paramString);
+Romo.prototype.selectPrev = function(elem, selector) {
+  // like `$().prev()`, but takes a selector; returns first prev elem that
+  // matches the given selector or an empty collection if non matches
+  var el = elem.prev();
+  while(el.length) {
+    if (selector === undefined || el.is(selector)) {
+      return el;
     }
-
-    return paramString;
+    el = el.prev();
   }
+  return el;
+}
 
-  Romo.prototype.decodeParam = function(string) {
-    return this.decodeParamMap.reduce(function(prev, curr) {
-      return prev.replace(curr[0], curr[1]);
-    }, string);
-  }
+// input handling
 
-  Romo.prototype.decodeParamMap = [
-    [/%20/g, '+'],
-    [/%21/g, '!'],
-    [/%24/g, '$'],
-    [/%28/g, '('],
-    [/%29/g, ')'],
-    [/%2A/g, '*'],
-    [/%2B/g, '+'],
-    [/%2C/g, ','],
-    [/%2D/g, '-'],
-    [/%2E/g, '.'],
-    [/%2F/g, '/'],
-    [/%5B/g, '['],
-    [/%5C/g, '\\'],
-    [/%5D/g, ']'],
-    [/%3A/g, ':'],
-    [/%3C/g, '<'],
-    [/%3E/g, '>'],
-    [/%3F/g, '?'],
-    [/%40/g, '@'],
-    [/%5E/g, '^'],
-    [/%5F/g, '_'],
-    [/%60/g, '`'],
-    [/%7B/g, '{'],
-    [/%7C/g, '|'],
-    [/%7D/g, '}'],
-    [/%7E/g, '~']
+Romo.prototype.nonInputTextKeyCodes = function() {
+  // https://css-tricks.com/snippets/javascript/javascript-keycodes/
+  return [
+    9,   /* tab */
+    13,  /* enter */
+    16,  /* shift */
+    17,  /* ctrl */
+    18,  /* alt */
+    19,  /* pausebreak */
+    20,  /* capslock */
+    27,  /* escape */
+    33,  /* pageup */
+    34,  /* pagedown */
+    35,  /* end */
+    36,  /* home */
+    37,  /* leftarrow */
+    38,  /* uparrow */
+    39,  /* rightarrow */
+    40,  /* downarrow */
+    45,  /* insert */
+    46,  /* delete */
+    91,  /* leftwindowkey */
+    92,  /* rightwindowkey */
+    93,  /* selectkey */
+    112, /* f1 */
+    113, /* f2 */
+    114, /* f3 */
+    115, /* f4 */
+    116, /* f5 */
+    117, /* f6 */
+    118, /* f7 */
+    119, /* f8 */
+    120, /* f9 */
+    121, /* f10 */
+    122, /* f11 */
+    123, /* f12 */
+    144, /* numlock */
+    145, /* scrolllock */
   ];
+}
 
-  // style handling
+// private
 
-  Romo.prototype.getComputedStyle = function(node, styleName) {
-    return window.getComputedStyle(node, null).getPropertyValue(styleName);
-  }
+Romo.prototype._addEventCallback = function(name, callback) {
+  this._eventCallbacks.push({ eventName: name, callback:  callback });
+}
 
-  Romo.prototype.parseZIndex = function(elem) {
-    // for the case where z-index is set directly on the elem
-    var val = this.parseElemZIndex(elem);
-    if (val !== 0) {
-      return val;
-    }
-
-    // for the case where z-index is inherited from a parent elem
-    var parentIndexes = this.toArray(elem.parents()).reduce($.proxy(function(prev, curr) {
-      var pval = this.parseElemZIndex($(curr));
-      if (pval !== 0) {
-        prev.push(pval);
-      }
-      return prev;
-    }, this), []);
-    parentIndexes.push(0); // in case z-index is 'auto' all the way up
-    return parentIndexes[0];
-  }
-
-  Romo.prototype.parseElemZIndex = function(elem) {
-    var val = parseInt(this.getComputedStyle(elem[0], "z-index"));
-    if (!isNaN(val)) {
-      return val;
-    }
-    return 0;
-  }
-
-  // elem handling
-
-  Romo.prototype.toArray = function(elems) {
-    // converts a collection of elements `$()` to an array of nodes
-    return $.map(elems, function(node){ return node; })
-  }
-
-  Romo.prototype.selectNext = function(elem, selector) {
-    // like `$().next()`, but takes a selector; returns first next elem that
-    // matches the given selector or an empty collection if non matches
-    var el = elem.next();
-    while(el.length) {
-      if (selector === undefined || el.is(selector)) {
-        return el;
-      }
-      el = el.next();
-    }
-    return el;
-  }
-
-  Romo.prototype.selectPrev = function(elem, selector) {
-    // like `$().prev()`, but takes a selector; returns first prev elem that
-    // matches the given selector or an empty collection if non matches
-    var el = elem.prev();
-    while(el.length) {
-      if (selector === undefined || el.is(selector)) {
-        return el;
-      }
-      el = el.prev();
-    }
-    return el;
-  }
-
-  // input handling
-
-  Romo.prototype.nonInputTextKeyCodes = function() {
-    // https://css-tricks.com/snippets/javascript/javascript-keycodes/
-    return [
-      9,   /* tab */
-      13,  /* enter */
-      16,  /* shift */
-      17,  /* ctrl */
-      18,  /* alt */
-      19,  /* pausebreak */
-      20,  /* capslock */
-      27,  /* escape */
-      33,  /* pageup */
-      34,  /* pagedown */
-      35,  /* end */
-      36,  /* home */
-      37,  /* leftarrow */
-      38,  /* uparrow */
-      39,  /* rightarrow */
-      40,  /* downarrow */
-      45,  /* insert */
-      46,  /* delete */
-      91,  /* leftwindowkey */
-      92,  /* rightwindowkey */
-      93,  /* selectkey */
-      112, /* f1 */
-      113, /* f2 */
-      114, /* f3 */
-      115, /* f4 */
-      116, /* f5 */
-      117, /* f6 */
-      118, /* f7 */
-      119, /* f8 */
-      120, /* f9 */
-      121, /* f10 */
-      122, /* f11 */
-      123, /* f12 */
-      144, /* numlock */
-      145, /* scrolllock */
-    ];
-  }
-
-  // private
-
-  Romo.prototype._addEventCallback = function(name, callback) {
-    this._eventCallbacks.push({ eventName: name, callback:  callback });
-  }
-
-  window.Romo = new Romo();
-})();
+window.Romo = new Romo();
 
 $(function() {
-
   Romo.doInit();
-
 })
 
 var RomoParentChildElems = function() {
